bed6ae630d547c6c827084c8b18ab89380775a6e
sarbutina20 <sarbutina20@foi.hr>
2023-09-10 22:53:08 +0200

    Dodana ruta i middleware.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,16 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res) {
+  dns.lookup(req.body.url, function(err, address, family) {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      res.json({ original_url: req.body.url, short_url: 1 });
+    }
+  });
 });
 
 app.listen(port, function() {
