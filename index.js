ce9440b38c1105f73917d238d2285da286f61f86
sarbutina20 <sarbutina20@foi.hr>
2023-09-10 23:02:01 +0200

    Napravljena redirekcija.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const dns = require('dns');
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,27 +10,37 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.post('/api/shorturl', function(req, res) {
-  dns.lookup(req.body.url, function(err, address, family) {
+const urlMap = new Map();
+const urlMapSize = urlMap.size;
+
+app.post("/api/shorturl", function (req, res) {
+  dns.lookup(req.body.url, function (err, address, family) {
     if (err) {
-      res.json({ error: 'invalid url' });
+      res.json({ error: "invalid url" });
     } else {
-      res.json({ original_url: req.body.url, short_url: 1 });
+      urlMap.set(urlMapSize, req.body.url);
+      res.json({ original_url: req.body.url, short_url: urlMapSize });
     }
   });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const url = urlMap.get(req.params.short_url);
+  if(!url) res.json({ error: "invalid url" });
+  res.redirect(url); 
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
